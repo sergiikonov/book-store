8bdac2cd5df19ea6247ffcf485892f67a49f4233
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-03-13 17:39:07 +0200

    created book-service & repo

---
+++ src/main/java/mate/academy/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,23 @@
+package mate.academy.bookstore.service;
+
+import java.util.List;
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
