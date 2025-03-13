8940f6d0667f67a37badc2c23df6f16ccae2eef9
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-03-13 17:49:11 +0200

    added imports in model

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -1,7 +1,10 @@
 package mate.academy.bookstore.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import lombok.Data;
 
