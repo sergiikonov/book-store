298b040cd332c0dcfc112084485818ecf70e992a
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-04-08 14:32:14 +0300

    added const

--- src/main/java/mate/academy/bookstore/repository/book/specs/IsbnSpecification.java
+++ src/main/java/mate/academy/bookstore/repository/book/specs/IsbnSpecification.java
@@ -8,13 +8,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class IsbnSpecification implements SpecificationProvider<Book> {
+    private static final String ISBN = "isbn";
+
     @Override
     public String getKey() {
-        return "isbn";
+        return ISBN;
     }
 
     public Specification<Book> getSpecification(String[] params) {
         return (root, query, criteriaBuilder)
-                -> root.get("isbn").in(Arrays.stream(params).toArray());
+                -> root.get(ISBN).in(Arrays.stream(params).toArray());
     }
 }
