c180126d8d86cb4c8acfd475aa69919add9db7e2
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-06-09 11:17:19 +0300

    added entity graph, fixed user model, added email to customuserdetails

--- src/main/java/mate/academy/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/mate/academy/bookstore/security/CustomUserDetailsService.java
@@ -1,10 +1,7 @@
 package mate.academy.bookstore.security;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import mate.academy.bookstore.model.User;
 import mate.academy.bookstore.repository.user.UserRepository;
-import org.hibernate.Hibernate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,13 +13,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("Can't find user by email")
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("Can't find user by email "
+                + email)
         );
-        Hibernate.initialize(user.getRoles());
-        return user;
     }
 }
